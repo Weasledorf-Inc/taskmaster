e115ea6a8a32b04d636bb1aed4df8ce1993a15b4
Hasan S. Abu-Rayyan <Hasanaburayyan21@gmail.com>
2021-10-09 17:07:10 -0400

    removed sub git module in cdk

---
+++ cdk-app/.projenrc.js
@@ -0,0 +1,18 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.125.0',
+  defaultReleaseBranch: 'main',
+  name: 'task-master',
+
+  cdkDependencies: [
+    '@aws-cdk/core',
+  ],
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // release: undefined,                /* Add release management to this project. */
+  github: false,
+});
+project.synth();
